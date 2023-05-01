2ff619afbad1a199fe4fc9ae4c2718e2ec91a8bb
nie0056 <martin.niemczyk@vsb.cz>
2023-05-01 14:18:51 +0200

    UPDATE minor changes

--- gradsolv/solv/_solvers.py
+++ gradsolv/solv/_solvers.py
@@ -23,41 +23,41 @@ class grad_solver(object):
 
         self._preconditioner = preconditioner
 
-    def _solve(self, matrix: ndarray, rhs: ndarray, err_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
+    def _solve(self, matrix: ndarray, rhs: ndarray, error_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
 
         raise NotImplementedError()
 
-    def solve(self, matrix: ndarray, rhs: ndarray, err_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
+    def solve(self, matrix: ndarray, rhs: ndarray, error_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
 
         error_occured = False
 
         if len(matrix.shape) != 2:
             
-            err_handle._set_error("Value of argument matrix must be 2-dimensional!")
+            error_handle._set_error("Value of argument matrix must be 2-dimensional!")
             error_occured = True
 
         elif matrix.shape[0] != matrix.shape[1]:
 
-            err_handle._set_error("Matrix must be square!")
+            error_handle._set_error("Matrix must be square!")
             error_occured = True
 
         elif len(rhs.shape) != 1:
             
-            err_handle._set_error("Value of argument rhs must be 1-dimensional!")
+            error_handle._set_error("Value of argument rhs must be 1-dimensional!")
 
         elif rhs.shape[0] != matrix.shape[0]:
             
-            err_handle._set_error("")
+            error_handle._set_error("")
             error_occured = True
 
         elif not _aux._check_matrix_symmetry(matrix):
             
-            err_handle._set_error("Matrix must be symmetric!")
+            error_handle._set_error("Matrix must be symmetric!")
             error_occured = True
 
         elif not _aux._check_symmetric_matrix_positive_definitness(matrix):
             
-            err_handle._set_error("Symmetric matrix must be positive definite!")
+            error_handle._set_error("Symmetric matrix must be positive definite!")
             error_occured = True
 
         if error_occured:
@@ -66,7 +66,7 @@ class grad_solver(object):
         
         else:
 
-            return self._solve(matrix, rhs, err_handle, tol, max_it, include_relres_vec)
+            return self._solve(matrix, rhs, error_handle, tol, max_it, include_relres_vec)
     
 class steepest_descend_solver(grad_solver):
 
@@ -74,7 +74,7 @@ class steepest_descend_solver(grad_solver):
 
         super().__init__()
 
-    def _solve(self, matrix: ndarray, rhs: ndarray, err_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
+    def _solve(self, matrix: ndarray, rhs: ndarray, error_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
         from numpy import array, zeros, dot
         from numpy.linalg import norm
 
@@ -111,7 +111,7 @@ class steepest_descend_solver(grad_solver):
 
         if (n_it == max_it) and ((norm_r/norm_r0) > tol):
 
-            err_handle._set_error("Max number of iterations was reached, but solver did not converge!")
+            error_handle._set_error("Max number of iterations was reached, but solver did not converge!")
 
         relres_vec = array(relres_vec) if include_relres_vec else None
 
@@ -123,7 +123,7 @@ class conjugate_gradient_solver(grad_solver):
 
         super().__init__()
 
-    def _solve(self, matrix: ndarray, rhs: ndarray, err_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
+    def _solve(self, matrix: ndarray, rhs: ndarray, error_handle: error_handle, tol: float, max_it: int, include_relres_vec=False) -> grad_solver_result:
         from numpy import array, zeros, dot
         from numpy.linalg import norm
 
@@ -171,7 +171,7 @@ class conjugate_gradient_solver(grad_solver):
 
         if (n_it == max_it) and ((norm_r/norm_r0) > tol):
 
-            err_handle._set_error("Max number of iterations was reached, but solver did not converge!")
+            error_handle._set_error("Max number of iterations was reached, but solver did not converge!")
 
         relres_vec = array(relres_vec) if include_relres_vec else None
 
